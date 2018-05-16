97e106db06b12ef06221496c7f7622dfb516f563
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-05-16 09:21:51 -0600

    Helper methods for secure TLS configuration and loading PEM from file.

--- tls.go
+++ tls.go
@@ -22,3 +22,25 @@ func NewTLSDialer(config *tls.Config) Dialer {
 func (this *TLSDialer) Dial(network, address string) (net.Conn, error) {
 	return tls.Dial(network, address, this.config)
 }
+
+func DefaultConfig() *tls.Config {
+	return &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		PreferServerCipherSuites: true,
+		SessionTicketsDisabled:   true,
+		CipherSuites: []uint16{
+			tls.TLS_FALLBACK_SCSV,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		},
+	}
+}
+func ConfigWithPEM(filename string) (*tls.Config, error) {
+	if cert, err := tls.LoadX509KeyPair(filename, filename); err != nil {
+		return nil, err
+	} else {
+		config := DefaultConfig()
+		config.Certificates = []tls.Certificate{cert}
+		return config, nil
+	}
+}
