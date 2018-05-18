31184cce987da95e0b3fa6a811e8111fc53aeb15
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-05-18 10:12:22 -0600

    Extracted shared code to be an io.WriteCloser interface.

---
+++ channel_writer.go
@@ -0,0 +1,99 @@
+package transports
+
+import (
+	"errors"
+	"io"
+	"sync/atomic"
+)
+
+type ChannelWriter struct {
+	dialer   Dialer
+	address  string
+	channel  chan []byte
+	closed   uint64
+	writer   io.WriteCloser
+	buffered [][]byte
+}
+
+func NewChannelWriter(dialer Dialer, address string, capacity int) *ChannelWriter {
+	this := &ChannelWriter{
+		dialer:  dialer,
+		address: address,
+		channel: make(chan []byte, capacity),
+	}
+	go this.listen()
+	return this
+}
+
+// NOTE: the buffer cannot be reused because it's being sent to a different goroutine
+func (this *ChannelWriter) Write(buffer []byte) (int, error) {
+	select {
+	case this.channel <- buffer:
+		return len(buffer), nil
+	default:
+		return 0, ErrChannelFull
+	}
+}
+func (this *ChannelWriter) Close() error {
+	// the client is designed to be written to and closed from
+	// exactly one goroutine which is why we shut down the channel
+	if atomic.AddUint64(&this.closed, 1) == 1 {
+		close(this.channel)
+	}
+	return nil
+}
+func (this *ChannelWriter) isClosed() bool {
+	return atomic.LoadUint64(&this.closed) > 0
+}
+
+func (this *ChannelWriter) listen() {
+	defer this.closeWriter()
+
+	for message := range this.channel {
+		this.buffered = append(this.buffered, message)
+		if len(this.channel) == 0 {
+			this.ensureWrite()
+		}
+	}
+}
+func (this *ChannelWriter) ensureWrite() {
+	for !this.isClosed() {
+		if !this.openWriter() {
+			continue
+		}
+
+		if this.writeBuffer() {
+			break // done
+		}
+
+		this.closeWriter()
+	}
+}
+func (this *ChannelWriter) writeBuffer() bool {
+	for _, message := range this.buffered {
+		if _, err := this.writer.Write(message); err != nil {
+			return false
+		}
+	}
+
+	this.buffered = this.buffered[0:0]
+	return true
+}
+func (this *ChannelWriter) openWriter() bool {
+	if this.writer != nil {
+		return true
+	} else if socket, err := this.dialer.Dial("tcp", this.address); err != nil {
+		return false
+	} else {
+		this.writer = socket
+		return true
+	}
+}
+func (this *ChannelWriter) closeWriter() {
+	if this.writer != nil {
+		this.writer.Close()
+		this.writer = nil
+	}
+}
+
+var ErrChannelFull = errors.New("channel full")
