0bfd7f0fadf6c0529d25e653dd2d79217f65cfe9
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-05-24 12:03:33 -0400

    Extracted to separate file.

--- tls_dialer.go
+++ tls_dialer.go
@@ -22,30 +22,11 @@ func (this TLSDialer) Dial(network, address string) (net.Conn, error) {
 	return tls.DialWithDialer(this.dialer, network, address, this.config)
 }
 
-func DefaultTLSConfig() *tls.Config {
-	return &tls.Config{
-		MinVersion:               tls.VersionTLS12,
-		PreferServerCipherSuites: true,
-		SessionTicketsDisabled:   true,
-		CipherSuites: []uint16{
-			tls.TLS_FALLBACK_SCSV,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		},
-	}
-}
-func TLSConfigWithPEM(filename string) (*tls.Config, error) {
-	if cert, err := tls.LoadX509KeyPair(filename, filename); err != nil {
-		return nil, err
-	} else {
-		config := DefaultTLSConfig()
-		config.Certificates = []tls.Certificate{cert}
-		return config, nil
-	}
-}
-
 type TLSDialerOption func(*TLSDialer)
 
 func WithTLSConfig(config *tls.Config) TLSDialerOption {
 	return func(this *TLSDialer) { this.config = config }
+}
+func WithTLSServerName(name string) TLSDialerOption {
+	return func(this *TLSDialer) { this.config.ServerName = name }
 }
